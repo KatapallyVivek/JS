7756cc000f3ac5bc1583460c97162e4554897eb0
madupojumounika <madupojumounika0@gmail.com>
2026-03-22 11:03:13 +0530

    Add DOM file and update functions

---
+++ dom.js
@@ -0,0 +1,34 @@
+/* dom --> it converts into tree structure (objects)[document object model]
+
+Browser converts it like:
+Document
+ └── html
+      └── body
+           └── h1 ("Hello")
+           
+javaScript can:
+Change text
+Change styles
+Add/remove elements
+
+
+types
+1.select Element
+2.add/remove element 
+3.creat element */
+
+
+//selecting with id
+document.getElementById("id name")
+//selecting with class
+document.getElementsByClassName("text")
+//selecting with tag
+document.getElementsByTagName("h1")
+
+//query selector
+document.querySelector("h1")
+
+document.querySelectorAll()
+
+
+
